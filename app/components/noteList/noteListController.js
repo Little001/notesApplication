b6555fc1f0c86f0c3e3b333c2e5809d78a461c25
martin <meravy.martin@gmail.com>
2016-03-31 23:08:33 +0200

    some changes

--- app/components/noteList/noteListController.js
+++ app/components/noteList/noteListController.js
@@ -9,9 +9,6 @@ angular.module('appControllers')
   .controller('noteListController', ['$scope','notes', 'Notification', function($scope, notes, Notification){
       
       $scope.notes = notes;
-      
-       $scope.error = function() {
-                    Notification.error('Error notification');
-                };
-        }
+
+   }
 ]);
